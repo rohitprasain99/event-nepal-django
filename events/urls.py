3a8db43013d25bd8aca4be77dfb19dc2f850285c
Rohit Prasain <prasainrohit@gmail.com>
2025-01-06 20:41:19 +0000

    feat: implement route handlers for event app

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create", views.create, name='create_event'),
+    path("get/<str:id>", views.getById, name='get_event'),
+    path("update/<str:id>", views.updateById, name='update_event'),
+    path("delete/<str:id>", views.deleteById, name='delete_event')
+
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+# from django.views.decorators.http import require_http_methods, require_GET, require_POST
 
 # Create your views here.
+events = [
+    {
+    "id":"1", 
+     "title":'first event', 
+     "date":"2017/2/2"
+     },
+       {
+    "id":"1a", 
+     "title":'second event', 
+     "date":"2017/3/2"
+     },
 
+]
 def index(request):
-    return HttpResponse('hello world, from views')
+    if request.method == 'GET':
+        return JsonResponse(events, safe=False) #json response expects dictionary data by default
+
+def create(request):
+    if request.method == 'POST':
+        return HttpResponse("event created successfully")
+
+def getById(request, id):
+    if request.method == 'GET':
+        return HttpResponse(f"event with id {id} has following details")
+
+def updateById(request, id):
+    if request.method == 'PATCH':
+        return HttpResponse(f"event with id {id} updated successfully")
+
+def deleteById(request, id):
+    if request.method == 'DELETE':
+        return HttpResponse(f"event with id {id} deleted successfully")
 
