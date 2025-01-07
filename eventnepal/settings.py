cb71586f526b31e62acfa210e259ad7c1a68f3cd
Rohit Prasain <prasainrohit@gmail.com>
2025-01-07 10:08:37 +0000

    feat: connect to db and implement model migration

--- eventnepal/settings.py
+++ eventnepal/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'events.apps.EventsConfig',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +76,17 @@ WSGI_APPLICATION = 'eventnepal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+ 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+
+        "USER": "postgres",
+        "PASSWORD": "p0stgres",
+
+        "NAME": "djangoevent",
     }
 }
 
--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Events(models.Model):
+    id = models.UUIDField(primary_key=True)
+    event_title = models.CharField(max_length=200)
+    event_volunteer = models.IntegerField(default=0)
+    event_date = models.DateTimeField()
+
